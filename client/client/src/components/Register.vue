8e5a2c7ff56479aa1dcefe6724187a2ab17c7651
JhyunJ1 <JhyunJ1005@gmail.com>
2023-08-26 19:08:32 +0900

    conf: start project django vue.js

---
+++ client/client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="Reg">
+<div class="UpperSide">
+<h3 id="name">회원가입</h3>
+</div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Welcom register'
+    }
+  }
+}
+</script>
+
+<style>
+.UpperSide {
+background-color: F6B590;
+width: 100%;
+height: 50px;
+margin-top: 0px;
+}
+.UpperSide #name {
+/* padding-top: 50%; */
+}
+</style>
